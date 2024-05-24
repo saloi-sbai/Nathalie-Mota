8d5ba74baa8966df30ba7c1bda200c9185931fe5
saloi sbai <saloi.benoum@gmail.com>
2024-05-24 23:28:08 +0200

    la mise en place du header et footer

---
+++ footer.php
@@ -0,0 +1,14 @@
+</main>
+<footer id="footer" role="contentinfo">
+    <?php
+    wp_nav_menu(array(
+        'footer menu' => 'footer_menu',
+    ));
+    ?>
+
+</footer>
+</div>
+
+<?php wp_footer() ?>
+
+</body>
