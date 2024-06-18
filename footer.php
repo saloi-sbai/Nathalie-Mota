e7441cd579521b3a51540b4c9923931a93cfcf3f
saloi sbai <saloi.benoum@gmail.com>
2024-06-19 00:20:01 +0200

    footer ok

--- footer.php
+++ footer.php
@@ -1,18 +1,21 @@
-</main>
 <footer id="footer" role="contentinfo">
     <?php
     wp_nav_menu(array(
         'footer menu' => 'footer_menu',
+        'container' => 'false',
+        'menu_class' => 'footer-nav',
     ));
     ?>
 
+    <!-- affichage de la modal -->
+    <?php get_template_part('template-parts/contact-modal'); ?>
+
+    <!-- affichage de la lightbox -->
+    <?php get_template_part('template-parts/lightbox'); ?>
+
 </footer>
 <!-- </div> -->
-<!-- affichage de la modal -->
-<?php get_template_part('template-parts/contact-modal'); ?>
 
-<!-- affichage de la lightbox -->
-<?php get_template_part('template-parts/lightbox'); ?>
 
 <?php wp_footer() ?>
 
